27f50b69482aa90b02384f99cf101aca70a1d48a
Jan Gazda <1oglop1@gmail.com>
2020-03-21 19:05:44 +0100

    Fix dev deployment to gh pages

--- publishconf_gh.py
+++ publishconf_gh.py
@@ -6,14 +6,14 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pyamsterdam.github.io/website-template'
+SITEURL = 'https://pyamsterdam.github.io/py.amsterdam'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
-CDN = True
+CDN = False
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
