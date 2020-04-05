7f41afdd23d0d32de31a43d7b2c8a5d11f991662
Jan Gazda <1oglop1@gmail.com>
2020-04-05 21:13:39 +0200

    Test the first deployment

---
+++ conf_preview.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://pyamsterdam.1oglop1.now.sh'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+CDN = True
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
